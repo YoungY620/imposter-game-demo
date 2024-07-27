fc569818f153b8ffcf2ce68eb4403e7bdba3e5ed
Young Yee <youngyee620@qq.com>
2024-07-27 23:56:53 +0800

    overall structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-[#1c1c1c] text-[#9acd32] font-mono">
       <body className={inter.className}>{children}</body>
     </html>
   );
